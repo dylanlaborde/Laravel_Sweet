a5a6461611751db0bbd198344886fb022a45df9a
Dylan <dylan4047@gmail.com>
2017-02-16 13:54:41 +0100

    rajout function affichage ajout suppresion ( requete php )

--- app/Http/Controllers/SweetController.php
+++ app/Http/Controllers/SweetController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Sweet;
 
 class SweetController extends Controller
 {
-    //
+	public function getIndex(){
+		$sweet = Sweet::all();
+		return view('sweet.index',['sweets'=>$sweet]);		
+	}
+	public function sellSweet($id){
+		$sweet=Sweet::find($id);
+		$sweet->qty--;
+		$sweet->save();
+		return back();
+
+	}
+	public function buySweet($id){
+		$sweet=Sweet::find($id);
+		$sweet->qty++;
+		$sweet->save();
+		return back();
+		
+	}
+	public function addSweet(Request $request){
+		$sweet = new Sweet;
+		$sweet->name = $request->name;
+		$sweet->qty = $request->qty;
+		$sweet->save();
+		return back();
+	}
+	public function deleteSweet($id){
+		$sweet=Sweet::find($id);
+		$sweet->delete();
+		return back();
+	}
 }
