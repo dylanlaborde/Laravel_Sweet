43f170a3b37cb020200c926618675c37e40591e9
Dylan <dylan4047@gmail.com>
2017-02-16 13:59:22 +0100

    ajout des route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','SweetController@getIndex');
+
+Route::post('/sell/{id}','SweetController@sellSweet');
+Route::post('/refill/{id}','SweetController@buySweet');
+Route::post('/add_sweet','SweetController@addSweet');
+
+Route::post('/delete/{id}','SweetController@deleteSweet');
