a0067e6901d1ed512f147166a6341e156888a879
Dylan <dylan4047@gmail.com>
2017-02-16 13:58:36 +0100

    suppr function test en php avant pasage en api/ajax

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+// Route::get('/',function(){
+// 	 return response()->json(Sweet::all());
+// });
